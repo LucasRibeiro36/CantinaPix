9b6e059b47c3ab591523fa07abfbd9b42f794f34
LucasRibeiro36 <lucasribeiro5569@gmail.com>
2023-12-03 04:03:48 -0300

    fix: adicionando porta

--- config/database.js
+++ config/database.js
@@ -8,6 +8,7 @@ const sequelize = new Sequelize(
   process.env.DATABASE_USER,
   process.env.DATABASE_PASSWORD, {
   host: process.env.DATABASE_HOST,
+  por: process.env.DATABASE_PORT,
   dialect: process.env.DATABASE_DIALECT || 'mysql',
 });
 
